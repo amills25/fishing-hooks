74f42bfe2ff57b4b22271f6c86a61c2a6b99011a
awmills25 <awmills25@gmail.com>
2021-10-27 20:12:41 -0400

    added checkout button and custom card border styling

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -12,11 +12,11 @@ export default function Cart(props) {
             </Container>
             <br></br>
             {props.cartArray.length > 0 ? (
-                <Container className="border-bottom border-dark">
+                <Container>
                     {props.cartArray.map((product, idx) => (
                         <Row
                             key={idx}
-                            className="border-top border-dark align-items-center"
+                            className="row-bordered align-items-center"
                         >
                             <Col>
                                 <Image
@@ -48,48 +48,18 @@ export default function Cart(props) {
                             </Col>
                         </Row>
                     ))}
+                    <Row>
+                        <Col className="text-center display-2">
+                            <Button
+                                variant="dark"
+                                // onClick={() => props.removeFromCart(product.id)}
+                            >
+                                CHECKOUT
+                            </Button>
+                        </Col>
+                    </Row>
                 </Container>
             ) : (
-                // <Container className="text-center">
-                //     <Row xs={1} md={3} className="g-4">
-                //         {props.cartArray.map((product, idx) => (
-                //             <Col key={idx}>
-                //                 <Card className="h-100">
-                //                     <Card.Body>
-                //                         <Card.Img
-                //                             variant="top"
-                //                             src={product.image}
-                //                         />
-                //                         <Card.Title>{product.name}</Card.Title>
-                //                         <Card.Subtitle>
-                //                             ${product.price}
-                //                         </Card.Subtitle>
-                //                         {/* <Card.Text>
-                //                             {product.description}
-                //                         </Card.Text> */}
-                //                         <br></br>
-                // <Button
-                //     as={Link}
-                //     to={`/item/${product.id}`}
-                //     variant="flush"
-                // >
-                //     View Item
-                // </Button>
-                // {" || "}
-                // <Button
-                //     variant="flush"
-                //     onClick={() =>
-                //         props.removeFromCart(product.id)
-                //     }
-                // >
-                //     Remove from Cart
-                // </Button>
-                //                     </Card.Body>
-                //                 </Card>
-                //             </Col>
-                //         ))}
-                //     </Row>
-                // </Container>
                 <Container className="display-2 text-center">
                     Your cart is empty.
                 </Container>
